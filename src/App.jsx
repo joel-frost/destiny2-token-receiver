fecb36a8959736c60f7e327a1d288829f2c053d5
joelfrost <joelfrost4@gmail.com>
2023-03-05 20:35:07 +0000

    Add link to local env

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,23 @@ function App() {
 
 
     const [token, setToken] = useState("Not received");
+    const [redirect, setRedirect] = useState("");
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         let currentToken = urlParams.get('code');
-        if (currentToken !== null) setToken(currentToken);
+        if (currentToken !== null) {
+            setToken(currentToken);
+            setRedirect("http://localhost:3000/?code="+currentToken);
+        }
+
     }, []);
     return (
     <div className="App">
       <h3>Token Receiver</h3>
       <br />
       <p>Received code {token}</p>
+        <a href={redirect}>Continue to local</a>
     </div>
   );
 }
