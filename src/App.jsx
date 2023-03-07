bdec95c4aa9f57f24e55b66f86346dda4456db61
joelfrost <joelfrost4@gmail.com>
2023-03-07 22:41:50 +0000

    Revert auth

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,22 @@ function App() {
 
     const [token, setToken] = useState("Not received");
     const [redirect, setRedirect] = useState("");
-    const [authToken, setAuthToken] = useState("");
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         let currentToken = urlParams.get('code');
         if (currentToken !== null) {
             setToken(currentToken);
-            let authRes = getAuthData();
             setRedirect("http://localhost:3000/login/?code="+authRes);
         }
 
     }, []);
-
-    const getAuthData = async () => {
-        let clientId = "43194";
-        const response = await axios.post(
-            "https://www.bungie.net/platform/app/oauth/token/",
-            `grant_type=authorization_code&code=${token}&client_id=${clientId}`,
-            {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                },
-            }
-        );
-        return response.data.access_token;
-    }
 
     return (
     <div className="App">
       <h3>Token Receiver</h3>
       <br />
       <p>Received code {token}</p>
-        <p>Received auth {authToken}</p>
         <a href={redirect}>Continue to local</a>
     </div>
   );
